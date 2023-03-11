853151b12c8a257efddd1eca58648a19add26231
Sohee <sohee@machie.local>
2023-03-10 18:14:40 -0800

    reset

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementsByID("menuButton")
-const naviList = document.getElementById("navi-list")
-
-menuButton.addEventListener('click', () => {
-    naviList.classList.toggle('active');
-})
